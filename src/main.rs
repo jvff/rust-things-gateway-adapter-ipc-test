541be2de4452cfefc8a8948891c1504c1ac021b3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-11-26 09:35:17 -0200

    Open addonManager IPC socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate nanomsg;
+
+use std::io::Read;
+
+use nanomsg::{Protocol, Socket};
+
 fn main() {
-    println!("Hello, world!");
+    let mut socket = Socket::new(Protocol::Rep).expect("Failed to open socket");
+    let mut endpoint = socket
+        .bind("ipc:///tmp/gateway.addonManager")
+        .expect("Failed to bind socket");
+    let mut string = String::new();
+
+    loop {
+        socket
+            .read_to_string(&mut string)
+            .expect("Failed to read from socket");
+        println!("{}", string);
+        string.clear();
+    }
+
+    endpoint.shutdown();
 }
