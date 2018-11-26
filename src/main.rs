cd639a3c67b295f12f24c2625cbc2aa790e87e64
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-11-26 14:53:29 -0200

    Open socket for each registered plugin

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate serde_json;
 
 use std::collections::HashSet;
 use std::io::Read;
+use std::thread;
 
 use nanomsg::{Protocol, Socket};
 
@@ -42,8 +43,11 @@ fn main() {
             serde_json::from_str(&message).expect("Failed to deserialize response");
         let PluginRegistrationRequest::RegisterPlugin { plugin_id } = request;
         let ipc_base_addr = format!("gateway.plugin.{}", plugin_id);
+        let plugin_socket_address = format!("ipc:///tmp/{}", ipc_base_addr);
 
         plugins.insert(plugin_id.clone());
+
+        thread::spawn(|| handle_plugin(plugin_socket_address));
 
         let reply = PluginRegistrationReply::RegisterPluginReply {
             plugin_id,
@@ -52,6 +56,24 @@ fn main() {
 
         serde_json::to_writer(&mut socket, &reply).expect("Failed to send reply");
 
+        message.clear();
+    }
+
+    endpoint.shutdown();
+}
+
+fn handle_plugin(socket_address: String) {
+    let mut socket = Socket::new(Protocol::Pair).expect("Failed to open socket for plugin");
+    let mut endpoint = socket
+        .bind(&socket_address)
+        .expect("Failed to bind plugin socket");
+    let mut message = String::new();
+
+    loop {
+        socket
+            .read_to_string(&mut message)
+            .expect("Failed to read from plugin socket");
+        println!("Plugin sent: {}", message);
         message.clear();
     }
 
